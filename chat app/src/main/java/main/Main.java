e2d880263ed983105ae508686675e7e83eb68c19
Sven <svenvdp@hotmail.nl>
2022-09-09 12:06:19 +0200

    switched from Swing to JavaFX

--- chat app/src/main/java/main/Main.java
+++ chat app/src/main/java/main/Main.java
@@ -1,14 +1,12 @@
 package main;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-
+import gui.Authentication;
+import gui.chatWindow;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,14 +16,15 @@ public class Main extends Application {
     private boolean arrayFilled = false ;
 	
     public static void main(String[] args) {
-        //launch(args);
-        AuthenticationScreen();
+        launch(args);
+        //AuthenticationScreen();
     }
     
     @Override
     public void start(final Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
-        Pane root = new Pane();
+        //Pane root = new Pane();
+        Authentication root = new Authentication(new VBox());
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
         
@@ -51,25 +50,5 @@ public class Main extends Application {
 
         frameRateMeter.start();
     }
-    
-    //login form source: https://www.javatpoint.com/login-form-java
-    public static void AuthenticationScreen() {
-    	try  
-        {  
-    		//create JPanel where the user should go after authentication
-    		JPanel nextPage = new JPanel();
-    		nextPage.setSize(1080,720);                         
-    		
-            //create instance of the Authentication      		
-    		
-            Authentication form = new Authentication(nextPage);  
-            form.setSize(1080,720);  //set size of the frame  
-            form.setVisible(true);  //make form visible to the user  
-        }  
-        catch(Exception e)  
-        {     
-            //handle exception   
-            JOptionPane.showMessageDialog(null, e.getMessage());  
-        }  
-    }
+     
 }
