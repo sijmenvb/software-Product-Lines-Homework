8ee56062ebc364dfba6104f4773e6331b08e1373
sijmen_v_b <sijmenvanbommel@gmail.com>
2022-09-18 18:22:23 +0200

    formatting

--- chat app/src/main/java/backend/Configuration.java
+++ chat app/src/main/java/backend/Configuration.java
@@ -1,15 +1,15 @@
 package backend;
 
-public  class Configuration {
-	//all arguments are case insensitive 
-	
-	//color selection true by default disabled by -disableColor
+public class Configuration {
+	// all arguments are case insensitive
+
+	// color selection true by default disabled by -disableColor
 	public boolean COLOR = true;
-	//logging default is logging can be set by debug using -debug
+	// logging default is logging can be set by debug using -debug
 	public boolean DEBUG = false;
-	//disable logging by using -nolog
+	// disable logging by using -nolog
 	public boolean LOGGING = true;
-	
+
 	public Configuration(String[] args) {
 		for (int i = 0; i < args.length; i++) {
 			switch (args[i].toLowerCase()) {
@@ -23,7 +23,7 @@ public  class Configuration {
 				LOGGING = false;
 				break;
 			default:
-				System.err.println(args[i]+ " is not a recognised argument!");
+				System.err.println(args[i] + " is not a recognised argument!");
 				break;
 			}
 		}
